e19c8e5f24aa76840ac976c01715b7635d3fc3a9
Ivan Charapanau <mail@av.codes>
2025-01-09 11:39:33 +0100

    chore: 100 tables per file

--- generate.ts
+++ generate.ts
@@ -6,8 +6,8 @@ import { faker } from "npm:@faker-js/faker";
 
 const __dirname = import.meta.dirname;
 const outputDir = path.join(__dirname, "migrations");
-const files = 20;
-const tablesPerFile = 40;
+const files = 10;
+const tablesPerFile = 100;
 
 async function main() {
   if (!fs.existsSync(outputDir)) {
